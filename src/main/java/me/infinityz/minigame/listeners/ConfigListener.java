7cad45d23a7ad500b15b749d371769f6a96fab3f
AleIV <jorgefcorralesmayorga@iMac-de-Jorge.local>
2020-10-22 18:18:43 -0600

    new criticals

--- src/main/java/me/infinityz/minigame/listeners/ConfigListener.java
+++ src/main/java/me/infinityz/minigame/listeners/ConfigListener.java
@@ -116,7 +116,7 @@ public class ConfigListener implements Listener {
             final var backToBaseDamage = damage / 1.5;
             final var damageDifferential = damage - backToBaseDamage;
             if (damageDifferential > 2)
-                e.setDamage(backToBaseDamage + 3);
+                e.setDamage(backToBaseDamage + 4);
 
         }
     }
